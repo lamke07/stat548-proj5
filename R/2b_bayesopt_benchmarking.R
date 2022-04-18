7b969cc7a9e8402f930f1e9ce05a9d15dfd1300c
lamke07 <62064274+lamke07@users.noreply.github.com>
2022-04-19 01:31:12 +0200

    Update 2b_bayesopt_benchmarking.R

--- R/2b_bayesopt_benchmarking.R
+++ R/2b_bayesopt_benchmarking.R
@@ -6,27 +6,19 @@ library(tidyverse)
 rm(list = ls())
 
 branin_data <- read_csv(here("data", "branin.csv"))
-fried_data <- read_csv(here("data", "fried.csv"))
-hig02_data <- read_csv(here("data", "hig02.csv"))
-holsetal13sin_data <- read_csv(here("data", "holsetal13sin.csv"))
+goldpr_data <- read_csv(here("data", "goldpr.csv"))
+hart3_data <- read_csv(here("data", "hart3.csv"))
+# hart6_data <- read_csv(here("data", "hart6.csv"))
+# fried_data <- read_csv(here("data", "fried.csv"))
+# hig02_data <- read_csv(here("data", "hig02.csv"))
+# holsetal13sin_data <- read_csv(here("data", "holsetal13sin.csv"))
 
 source(here("R", "0b_functions_data.R"))
 source(here("R", "0a_functions_utils.R"))
 source(here("R", "0c_functions_BayesOpt.R"))
 
-
+dir.create(here("output"))
 ################################################################################
-################################################################################
-# Multiple data sets
-# SET MAX ITER 500
-# Cases
-# PowerExponential/Matern
-# Fit/BayesianFit (Full/Hybrid)
-# BayesianPredict (True/False)
-
-# Improvement threshold: 0.01
-# nstarts: 50
-
 # Set of parameters for frequentist BayesOpt
 par_freq_BayesOpt <- expand.grid(
   cor_family = c("PowerExponential", "Matern")) %>%
@@ -36,18 +28,26 @@ par_freq_BayesOpt <- expand.grid(
 par_bayesian_BayesOpt <- expand.grid(
   cor_family = c("PowerExponential", "Matern"),
   bayesian_method = c("Full", "Hybrid"),
-  bayesian_predict = c(TRUE, FALSE)) %>%
+  bayesian_predict = c(TRUE)) %>%
   data.frame()
 
 par_bayesian_BayesOpt_split <- asplit(par_bayesian_BayesOpt, MARGIN = 2)
 par_bayesian_BayesOpt_split$bayesian_predict <- as.logical(par_bayesian_BayesOpt$bayesian_predict)
 
+################################################################################
+# Benchmark functions
+n_starts <- 20
+max_iter <- 500
+improvement_threshold <- 10^-6
+
+# frequentist benchmark
 benchmark_freq_BayesOpt <- function(data_x, data_y, f_obj, par_bounds, par_BayesOpt, run_nr){
+  
   res <- purrr::pmap(asplit(par_BayesOpt, MARGIN = 2),
                      ~purrr::safely(BayesOpt_timed)(
                        data_x = data_x, data_y = data_y, f_obj = f_obj,
                        reg_model = ~1, cor_family = ..1,
-                       par_bounds = par_bounds, n_starts = 100, max_iter = 500, improvement_threshold = 0.000001,
+                       par_bounds = par_bounds, n_starts = n_starts, max_iter = max_iter, improvement_threshold = improvement_threshold,
                        run_nr = run_nr
                      ))
   
@@ -60,7 +60,7 @@ benchmark_bayesian_BayesOpt <- function(data_x, data_y, f_obj, par_bounds, par_B
                        data_x = data_x, data_y = data_y, f_obj = f_obj,
                        reg_model = ~1, cor_family = ..1,
                        bayesian_fit = TRUE, bayesian_method = ..2, bayesian_predict = ..3,
-                       par_bounds = par_bounds, n_starts = 20, max_iter = 500, improvement_threshold = 0.000001,
+                       par_bounds = par_bounds, n_starts = n_starts, max_iter = max_iter, improvement_threshold = improvement_threshold,
                        run_nr = run_nr
                      ))
   
@@ -68,28 +68,75 @@ benchmark_bayesian_BayesOpt <- function(data_x, data_y, f_obj, par_bounds, par_B
 }
 
 ################################################################################
+no_of_BayesOpt <- 1
 ################################################################################
+data_select <- branin_data
+f_obj <- branin
+
 par_bounds_x <- rbind(c(-5, 10),
                       c(0, 15)) %>%
   data.frame()
 colnames(par_bounds_x) <- c("lower", "upper")
 
-x1 <- purrr::map(1:5,
-                ~benchmark_freq_BayesOpt(data_x = select(branin_data, -y), data_y = select(branin_data, y),  f_obj = branin,
+branin_res_freq <- purrr::map(1:no_of_BayesOpt,
+                ~benchmark_freq_BayesOpt(data_x = select(data_select, -y), data_y = select(data_select, y),  f_obj = f_obj,
                                         par_bounds = par_bounds_x, par_BayesOpt = par_freq_BayesOpt, run_nr = .x))
-x2 <- purrr::map(1:5,
-                ~benchmark_bayesian_BayesOpt(data_x = select(branin_data, -y), data_y = select(branin_data, y),  f_obj = branin,
+
+saveRDS(branin_res_freq, here("output", "branin_res_freq.rds"))
+
+branin_res_bayes <- purrr::map(1:no_of_BayesOpt,
+                ~benchmark_bayesian_BayesOpt(data_x = select(data_select, -y), data_y = select(data_select, y),  f_obj = f_obj,
                                         par_bounds = par_bounds_x, par_BayesOpt = par_bayesian_BayesOpt_split, run_nr = .x))
 
+saveRDS(branin_res_bayes, here("output", "branin_res_bayes.rds"))
 
+# purrr::map_df(res_freq, ~purrr::map_df(.x, ~.x$result$out_summary))
+# purrr::map_df(x2, ~purrr::map_df(.x, ~.x$result$out_summary))
+# 
+# purrr::map_df(x1, ~.x$result$out_summary)
 
-x <- benchmark_freq_BayesOpt(data_x = select(branin_data, -y), data_y = select(branin_data, y),  f_obj = branin,
-                             par_bounds = par_bounds_x, par_BayesOpt = par_freq_BayesOpt)
+# purrr::map(x, ~.x$result$y)
+# purrr::map(x, ~round(.x$result$improvement_tracker, 6))
 
+################################################################################
+data_select <- goldpr_data
+f_obj <- goldpr
 
-purrr::map_df(x1, ~purrr::map_df(.x, ~.x$result$out_summary))
+par_bounds_x <- rbind(c(-2, 2),
+                      c(-2, 2)) %>%
+  data.frame()
+colnames(par_bounds_x) <- c("lower", "upper")
 
-purrr::map_df(x1, ~.x$result$out_summary)
+goldpr_res_freq <- purrr::map(1:no_of_BayesOpt,
+                       ~benchmark_freq_BayesOpt(data_x = select(data_select, -y), data_y = select(data_select, y),  f_obj = f_obj,
+                                                par_bounds = par_bounds_x, par_BayesOpt = par_freq_BayesOpt, run_nr = .x))
 
-purrr::map(x, ~.x$result$y)
-purrr::map(x, ~round(.x$result$improvement_tracker, 6))
+saveRDS(goldpr_res_freq, here("output", "goldpr_res_freq.rds"))
+
+goldpr_res_bayes <- purrr::map(1:no_of_BayesOpt,
+                        ~benchmark_bayesian_BayesOpt(data_x = select(data_select, -y), data_y = select(data_select, y),  f_obj = f_obj,
+                                                     par_bounds = par_bounds_x, par_BayesOpt = par_bayesian_BayesOpt_split, run_nr = .x))
+
+saveRDS(goldpr_res_bayes, here("output", "goldpr_res_bayes.rds"))
+
+################################################################################
+data_select <- hart3_data
+f_obj <- hart3
+
+par_bounds_x <- rbind(c(0, 1),
+                      c(0, 1),
+                      c(0, 1)) %>%
+  data.frame()
+colnames(par_bounds_x) <- c("lower", "upper")
+
+hart3_res_freq <- purrr::map(1:no_of_BayesOpt,
+                       ~benchmark_freq_BayesOpt(data_x = select(data_select, -y), data_y = select(data_select, y),  f_obj = f_obj,
+                                                par_bounds = par_bounds_x, par_BayesOpt = par_freq_BayesOpt, run_nr = .x))
+
+saveRDS(hart3_res_freq, here("output", "hart3_res_freq.rds"))
+
+hart3_res_bayes <- purrr::map(1:no_of_BayesOpt,
+                        ~benchmark_bayesian_BayesOpt(data_x = select(data_select, -y), data_y = select(data_select, y),  f_obj = f_obj,
+                                                     par_bounds = par_bounds_x, par_BayesOpt = par_bayesian_BayesOpt_split, run_nr = .x))
+
+saveRDS(hart3_res_bayes, here("output", "hart3_res_bayes.rds"))
