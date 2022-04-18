c14cdf71fd5577d90f92616c37efa7772afdf451
lamke07 <62064274+lamke07@users.noreply.github.com>
2022-04-19 01:08:44 +0200

    Update 0b_functions_data.R

--- R/0b_functions_data.R
+++ R/0b_functions_data.R
@@ -47,6 +47,173 @@ branin <- function(x1, x2, a=1, b=5.1/(4*pi^2), c=5/pi, r=6, s=10, t=1/(8*pi), .
   return(y)
 }
 
+goldpr <- function(x1, x2)
+{
+  ##########################################################################
+  #
+  # GOLDSTEIN-PRICE FUNCTION
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUT:
+  #
+  # xx = c(x1, x2)
+  #
+  ##########################################################################
+  
+  # x1 <- xx[1]
+  # x2 <- xx[2]
+  
+  fact1a <- (x1 + x2 + 1)^2
+  fact1b <- 19 - 14*x1 + 3*x1^2 - 14*x2 + 6*x1*x2 + 3*x2^2
+  fact1 <- 1 + fact1a*fact1b
+  
+  fact2a <- (2*x1 - 3*x2)^2
+  fact2b <- 18 - 32*x1 + 12*x1^2 + 48*x2 - 36*x1*x2 + 27*x2^2
+  fact2 <- 30 + fact2a*fact2b
+  
+  y <- fact1*fact2
+  return(y)
+}
+
+hart3 <- function(x1, x2, x3)
+{
+  ##########################################################################
+  #
+  # HARTMANN 3-DIMENSIONAL FUNCTION
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUT:
+  #
+  # xx = c(x1, x2, x3)
+  #
+  ##########################################################################
+  
+  alpha <- c(1.0, 1.2, 3.0, 3.2)
+  
+  A <- c(3.0, 10, 30,
+         0.1, 10, 35,
+         3.0, 10, 30,
+         0.1, 10, 35)
+  A <- matrix(A, 4, 3, byrow=TRUE)
+  P <- 10^(-4) * c(3689, 1170, 2673,
+                   4699, 4387, 7470,
+                   1091, 8732, 5547,
+                   381, 5743, 8828)
+  P <- matrix(P, 4, 3, byrow=TRUE)
+  
+  xxmat <- matrix(rep(c(x1, x2, x3),times=4), 4, 3, byrow=TRUE)
+  inner <- rowSums(A[,1:3]*(xxmat-P[,1:3])^2)
+  outer <- sum(alpha * exp(-inner))
+  
+  y <- -outer
+  return(y)
+}
+
+hart6 <- function(x1, x2, x3, x4, x5, x6)
+{
+  ##########################################################################
+  #
+  # HARTMANN 6-DIMENSIONAL FUNCTION
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUT:
+  #
+  # xx = c(x1, x2, x3, x4, x5, x6)
+  #
+  ##########################################################################
+  
+  alpha <- c(1.0, 1.2, 3.0, 3.2)
+  A <- c(10, 3, 17, 3.5, 1.7, 8,
+         0.05, 10, 17, 0.1, 8, 14,
+         3, 3.5, 1.7, 10, 17, 8,
+         17, 8, 0.05, 10, 0.1, 14)
+  A <- matrix(A, 4, 6, byrow=TRUE)
+  P <- 10^(-4) * c(1312, 1696, 5569, 124, 8283, 5886,
+                   2329, 4135, 8307, 3736, 1004, 9991,
+                   2348, 1451, 3522, 2883, 3047, 6650,
+                   4047, 8828, 8732, 5743, 1091, 381)
+  P <- matrix(P, 4, 6, byrow=TRUE)
+  
+  xxmat <- matrix(rep(c(x1, x2, x3, x4, x5, x6),times=4), 4, 6, byrow=TRUE)
+  inner <- rowSums(A[,1:6]*(xxmat-P[,1:6])^2)
+  outer <- sum(alpha * exp(-inner))
+  
+  y <- -(2.58 + outer) / 1.94
+  return(y)
+}
+
+
+
+
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# Other functions checked
 
 fried <- function(x1, x2, x3, x4, x5)
 {
